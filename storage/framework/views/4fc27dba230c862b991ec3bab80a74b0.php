13bdf97c255ec572f3bd33152e962dc9579183e3
xyaya26 <108610792+xyaya26@users.noreply.github.com>
2023-06-17 10:30:41 +0700

    Add files via upload

---
+++ storage/framework/views/4fc27dba230c862b991ec3bab80a74b0.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\FTIB - LAB DIGIN 18\Documents\praktikum_4\praktikum_4\praktikum_4\resources\views/profile.blade.php ENDPATH**/ ?>
